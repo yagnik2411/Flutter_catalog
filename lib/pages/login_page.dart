9a09f59a40107ff9e725ee421b7fb4508fdaf9ba
yagnik panchal <panchalyagnik2411@gmail.com>
2023-05-22 22:55:23 +0530

    chapter 7:finish

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/utils/MyRoutes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String _name = "";
+  bool changeButton = false;
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        changeButton = true;
+      });
+      // ignore: prefer_const_constructors
+      await Future.delayed(Duration(milliseconds: 1100));
+
+      // ignore: use_build_context_synchronously
+      await Navigator.pushNamed(context, MyRoutes.homeRoute);
+      setState(() {
+        changeButton = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,14 +38,16 @@ class LoginPage extends StatelessWidget {
           backgroundColor: Colors.deepPurpleAccent,
         ),
         body: SingleChildScrollView(
+            child: Form(
+          key: _formKey,
           child: Column(
             children: [
               Image.asset("assets/images/Login_page.png",
-                  fit: BoxFit.cover, height:300),
+                  fit: BoxFit.cover, height: 300),
               // ignore: prefer_const_constructors
               SizedBox(height: 30),
               // ignore: prefer_const_constructors
-              Text("Welcome",
+              Text("Welcome $_name",
                   // ignore: prefer_const_constructors
                   style: TextStyle(
                       fontSize: 24,
@@ -37,6 +64,17 @@ class LoginPage extends StatelessWidget {
                         hintText: "Enter Username",
                         labelText: "Username",
                       ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Username cannot be empty";
+                        }
+                        return null;
+                      },
+                      onChanged: (value) {
+                        setState(() {
+                          _name = value;
+                        });
+                      },
                     ),
                     TextFormField(
                       obscureText: true,
@@ -45,22 +83,50 @@ class LoginPage extends StatelessWidget {
                         hintText: "Enter Password",
                         labelText: "Password",
                       ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Password cannot be empty";
+                        } else if (value.length < 6) {
+                          return "Password length should be atleast 6";
+                        }
+                        return null;
+                      },
                     ),
                     // ignore: prefer_const_constructors
                     SizedBox(height: 10),
-                    ElevatedButton(
-                      child: const Text("Login"),
-                      // ignore: prefer_const_constructors
-                      style: ButtonStyle(minimumSize: MaterialStateProperty.all(Size(150, 40))),
-                      onPressed: () {
-                        Navigator.pushNamed(context, MyRoutes.homeRoute);
-                      },
+                    Material(
+                      color: Colors.deepPurpleAccent,
+                      borderRadius:
+                          BorderRadius.circular(changeButton ? 40 : 8),
+                      child: InkWell(
+                        onTap: () => moveToHome(context),
+                        child: AnimatedContainer(
+                          duration: Duration(seconds: 1),
+                          height: 40,
+                          width: changeButton ? 40 : 150,
+                          alignment: Alignment.center,
+                          // ignore: prefer_const_constructors, sort_child_properties_last
+                          child: changeButton
+                              // ignore: prefer_const_constructors
+                              ? Icon(
+                                  Icons.done,
+                                  color: Colors.white,
+                                )
+                              // ignore: prefer_const_constructors
+                              : Text(
+                                  "Login",
+                                  // ignore: prefer_const_constructors
+                                  style: TextStyle(
+                                      fontSize: 20, color: Colors.white),
+                                ),
+                        ),
+                      ),
                     )
                   ],
                 ),
               )
             ],
           ),
-        ));
+        )));
   }
 }

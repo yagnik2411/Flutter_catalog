dfd6c3eb62344a741a2977780e5385ad21cea2f6
yagnik panchal <panchalyagnik2411@gmail.com>
2023-06-02 11:15:27 +0530

    chapter 27:finish

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,22 +1,12 @@
 class CatalogModel {
+  static List<Item> items = [];
 
-
-  static List<Item> items = [
-    Item(
-        id: 1,
-        name: "iPhone 12 Pro",
-        desc: "Apple iPhone 12th generation",
-        price: 999,
-        color: "#33505a",
-        image:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
-  ];
   //get item by id
-   Item getById(int id) =>
+  Item getById(int id) =>
       items.firstWhere((element) => element.id == id, orElse: null);
 
   //get item by position
-   Item getByPosition(int pos) => items[pos];
+  Item getByPosition(int pos) => items[pos];
 }
 
 class Item {
